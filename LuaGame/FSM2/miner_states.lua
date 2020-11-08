e79de3bdf8760f446731a4bb1d78691cecb7d675
alb <albizgil@gmail.com>
2020-11-08 17:48:25 +0330

    Prettify!

--- LuaGame/FSM2/miner_states.lua
+++ LuaGame/FSM2/miner_states.lua
@@ -4,11 +4,11 @@ States =
 	{
 		name = "S1",
 		Enter = function(miner)
-			print ("[Lua]: Walkin' home in the hot n' thusty heat of the desert")
+			print ("[Lua@GoHome]: Walkin' home in the hot n' thusty heat of the desert")
 		end,
 
 		Execute = function(miner)
-			 print ("[Lua]: Back at the shack. Yes siree!")
+			 print ("[Lua@GoHome]: Back at the shack. Yes siree!")
 
 			if miner:Fatigued() then
 				miner:GetFSM():ChangeState(States.Sleep)
@@ -18,7 +18,7 @@ States =
 		end,
 
 		Exit = function(miner)
-			print ("[Lua]: Puttin' mah boots on n' gettin' ready for a day at the mine")
+			print ("[Lua@GoHome]: Puttin' mah boots on n' gettin' ready for a day at the mine")
 		end
 	},
 
@@ -26,12 +26,12 @@ States =
 	{
 		name = "S2",
 		Enter = function(miner)
-			print ("[Lua]: Miner " .. miner:Name() .." is dozin' off")	
+			print ("[Lua@Sleep]: Miner " .. miner:Name() .." is dozin' off")	
 		end,
 	
 		Execute = function(miner)
 			if miner:Fatigued() then
-				print ("[Lua]: ZZZZZZ... ")
+				print ("[Lua@Sleep]: ZZZZZZ... ")
 				miner:DecreaseFatigue()
 			else
 				miner:GetFSM():ChangeState(States.GoToMine)
@@ -39,7 +39,7 @@ States =
 		end,
 	
 		Exit = function(miner)
-			print ("[Lua]: Miner " .. miner:Name() .." is feelin' mighty refreshed!")
+			print ("[Lua@Sleep]: Miner " .. miner:Name() .." is feelin' mighty refreshed!")
 		end
 	},
 
@@ -47,22 +47,22 @@ States =
 	{
 		name = "S3",
 		Enter = function(miner)
-			print ("[Lua]: Miner " .. miner:Name() .." enters gold mine")
+			print ("[Lua@GoToMine]: Miner " .. miner:Name() .." enters gold mine")
 		end,
 
 		Execute = function(miner)
 			miner:IncreaseFatigue()
 			miner:AddGold(2)
-			print ("[Lua]: Miner " .. miner:Name() .." has got " .. miner:GetGold() .. " nuggets")
+			print ("[Lua@GoToMine]: Miner " .. miner:Name() .." has got " .. miner:GetGold() .. " nuggets")
 
 			if miner:GetGold() > 4 then
-				print ("[Lua]: Miner " .. miner:Name() .. " decides to go home, with his pockets full of nuggets")
+				print ("[Lua@GoToMine]: Miner " .. miner:Name() .. " decides to go home, with his pockets full of nuggets")
 				miner:GetFSM():ChangeState(States.GoHome)				
 			end
 		end,
 
 		Exit = function(miner)
-			print ("[Lua]: Miner " .. miner:Name() .. " exits gold mine")			
+			print ("[Lua@GoToMine]: Miner " .. miner:Name() .. " exits gold mine")			
 		end
 	}
 }
