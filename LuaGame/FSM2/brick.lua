42e6c13793adb19299dd351da6e3c3ce25c55b07
alb <albizgil@gmail.com>
2020-12-19 14:27:46 +0330

    moved instantiation code to utility.lua

---
+++ LuaGame/FSM2/brick.lua
@@ -0,0 +1,17 @@
+prefab =
+{
+	brick = 
+	{
+		visual =
+		{
+			width = 20,
+			height = 80,
+			color = {0, 255, 128},
+			hasCollider = true,
+			isDynamic = true
+		},		
+		brain = {script = "plainbox"}
+	}
+}
+
+return prefab
--- LuaGame/FSM2/utility.lua
+++ LuaGame/FSM2/utility.lua
@@ -18,4 +18,37 @@ function m.getKeys(t)
     return s 
 end
 
+m.instantiate = function(scene)
+	for entity, ent_data in pairs(scene) do
+		ret = EntMan:CreateEntity(entity)
+		for component, comp_data in pairs(ent_data) do
+			print ("component " .. component)		
+			if component == "visual" then
+				visual = EntMan:CreateRenderComponent(comp_data.width, comp_data.height, comp_data.color[1], comp_data.color[2], comp_data.color[3])			
+				RenderMan:Add(visual)
+				ret:AddComponent(visual)
+	
+				if comp_data.hasCollider == true then
+					print ("has collider")
+					collider = EntMan:CreateColliderComponent(comp_data.width, comp_data.height, comp_data.isDynamic)
+					CollisionMan:Add(collider, comp_data.isDynamic)	-- These three lines (as well as their counterparts in the previous bit) need to be moved to C and unified
+					ret:AddComponent(collider)	-- These three lines (as well as their counterparts in the previous bit) need to be moved to C and unified
+				end
+	
+			elseif component == "brain" then
+				lua = EntMan:CreateLuaComponent()
+				
+				script = comp_data.script			
+				args = comp_data.args			
+				lua_module = require(script)
+				brain = lua_module:new(args)
+	
+				lua:SetLuaBrain(brain)
+				brain:SetEntity(ret)
+				ret:AddComponent(lua)
+			end
+		end
+	end
+end
+
 return m
